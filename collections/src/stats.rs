e972625b29cc3273151313dff082b79427c3c012
Killinq <andrewalbert1808@gmail.com>
2022-04-23 14:28:33 -0400

    stats!

---
+++ collections/src/stats.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+use std::io;
+
+fn main() {
+    let mut vec: Vec<i32> = Vec::new();
+    add(&mut vec);
+}
+
+fn add(v: &mut Vec<i32>) {
+    let mut insert = String::new(); // hold the input
+    loop {
+        println!("Type a number or `done`: ");
+        io::stdin().read_line(&mut insert).expect("failed"); // read from stdin
+        if insert.trim().eq("done") {
+            stats(v);
+            break;
+        }
+        let num: i32 = match insert.trim().parse() {
+            // convert string to int
+            Ok(num) => num,
+            Err(_) => -1,
+        };
+        insert.clear();
+        v.push(num);
+    }
+}
+
+fn stats(v: &Vec<i32>) {
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for i in v {
+        let count = map.entry(*i).or_insert(0);
+        *count += 1;
+    }
+    println!("The median is: {}", v[v.len() / 2]);
+    for (key, val) in map {
+        println!("Num: {} Count: {}", key, val);
+    }
+    return;
+}
